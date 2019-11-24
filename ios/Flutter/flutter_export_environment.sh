5a254892fe5b61cdbda8c2c65152f3c2ee26b89d
riverlaker <riverlaker@foxmail.com>
2019-11-24 13:15:12 +0800

    提交导入信息

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/xj.deng/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/xj.deng/alibaba/xj_fluttergo/flutter-go"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/riverlaker/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/riverlaker/go/src/BimTraffic/flutter-go"
+export "FLUTTER_TARGET=/Users/riverlaker/go/src/BimTraffic/flutter-go/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/xj.deng/development/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/riverlaker/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.6"
 export "FLUTTER_BUILD_NUMBER=1.0.6"
